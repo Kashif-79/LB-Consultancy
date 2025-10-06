811fa6d85931804ecc7db86041c58e33c7b48f69
Kashif-79 <kashif.r.reza@gmail.com>
2025-10-06 06:17:23 +0600

    Vercel Express Handling

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import app from './app';
-import config from './app/config';
-import mongoose from 'mongoose';
-import seedSuperAdmin from './app/DB';
+// import app from './app';
+// import config from './app/config';
+// import mongoose from 'mongoose';
+// import seedSuperAdmin from './app/DB';
 
-async function main() {
-  try {
-    await mongoose.connect(config.database_url as string);
+// async function main() {
+//   try {
+//     await mongoose.connect(config.database_url as string);
 
-    seedSuperAdmin();
-    app.listen(config.port, () => {
-      console.log(`app listening on port ${config.port}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     seedSuperAdmin();
+//     app.listen(config.port, () => {
+//       console.log(`app listening on port ${config.port}`);
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-main();
+// main();
