0c28a3026110d87fee01f25003063c11678c52b4
Kashif-79 <kashif.r.reza@gmail.com>
2025-10-11 02:32:28 +0600

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,25 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+// app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+
+const allowedOrigins = [
+  'http://localhost:5173', // local dev
+  'https://lb-consultancy-frontend.vercel.app', // production frontend
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // allow cookies
+  }),
+);
 
 app.use('/api/v1', router);
 
